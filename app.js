092c4af725845324e00fba4535b8c7ca8428e5f5
Mindi Torrey <minditorrey@me.com>
2016-08-24 09:05:48 -0400

    add babel

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
+var babel = require('babel-polyfill');
 
 
 const MONGOURL = process.env.MONGODB_URI || 'mongodb://localhost/medicalethicsApp';
